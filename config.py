9ce9e7d63255e96c2c7f48f641c0a2c73ea90155
Bram <gitkraken@bram.blmgroep.nl>
2021-06-27 21:24:20 +0200

    Add configuration settings

---
+++ config.py
@@ -0,0 +1,14 @@
+# Minecraft bridge credentials
+MATRIX_HOMESERVER = "https://homeserv.er"
+MATRIX_USERNAME   = "@bridge_username:homeserv.er"
+MATRIX_PASSWORD   = "bridge_password"
+
+# Matrix users who are allowed to run OP commands in Minecraft through Matrix
+MC_ADMINS = [
+    "@bramvdnheuvel:nltrix.net",                # Bram on Matrix  (example, feel free to remove)
+    "@_discord_625632515314548736:t2bot.io"     # Bram on Discord (example, feel free to remove)
+    # Your username on Matrix
+]
+
+# Matrix channel that the bot should talk to
+MC_CHANNEL = "!channel_id:homeserv.er"
